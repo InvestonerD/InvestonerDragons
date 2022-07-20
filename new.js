615d06cc2e73d03fce70259df887c15b141a6d97
InvestonerD <solalanddao@gmail.com>
2022-07-19 19:40:23 -0700

    update

---
+++ new.js
@@ -0,0 +1,205 @@
+
+
+
+const body = document.querySelector("body"),
+    sidebar = document.querySelector(".sidebar"),
+    toggle = document.querySelector(".toggle"),
+    conn = document.querySelector(".connect"),
+    disconn = document.querySelector(".disconnect"),
+    user_balance = document.querySelector(".bottom-menu");
+phone_balance = document.querySelector("#b_phone");
+phone_balanceoff = document.querySelector("#bo_phone");
+phone_menuoff = document.querySelector("#bo_menu");
+phone_menu = document.querySelector("#s_phone");
+balances = document.querySelector(".balances");
+
+toggle.addEventListener("click", () => {
+    sidebar.classList.toggle("close");
+});
+
+disconn.addEventListener("click", () => {
+    user_balance.classList.toggle("expand");
+});
+
+phone_balance.addEventListener("click", () => {
+    balances.classList.toggle("phone");
+});
+
+phone_balanceoff.addEventListener("click", () => {
+    balances.classList.toggle("phone");
+});
+
+phone_menu.addEventListener("click", () => {
+    sidebar.classList.toggle("phone");
+});
+
+phone_menuoff.addEventListener("click", () => {
+    sidebar.classList.toggle("phone");
+});
+
+
+const lamports_per_sol = solanaWeb3.LAMPORTS_PER_SOL;
+
+
+function phantom_connect() {
+    var provider = () => {
+        if ("solana" in window) {
+            var provider = window.solana;
+            if (provider.isPhantom) {
+                return provider;
+            } else {
+                return false;
+            }
+        }
+        window.open("https://phantom.app", "_blank");
+    };
+
+    
+    var phantom = provider();
+
+    if (phantom !== false) {
+        console.log("Phantom Wallet Found, Connecting..");
+
+        try {
+            
+            var connect_wallet = phantom.connect();
+
+            phantom.on("connect", () => {
+                conn.addEventListener("click", () => {
+                    user_balance.classList.toggle("expand");
+                });
+
+                console.log("Phantom Connected: " + phantom.isConnected);
+
+                const wallet = phantom.publicKey;
+                var wallet_address = phantom.publicKey.toString();
+                console.log("Solana Wallet Address: " + wallet_address);
+                var first = wallet_address.substring(0, 5);
+                document.getElementById("wallet-address").innerHTML = first + "...";
+
+            (async () => {
+                const connection = new solanaWeb3.Connection(solanaWeb3.clusterApiUrl("mainnet-beta"),
+                    'confirmed',
+                );
+
+                if (solana && solana.publicKey !== null) {
+                    const tokenAccount = new solanaWeb3.PublicKey(
+                        "J3Lw33iBvMLHdCua4MXohTx3HD4JcajQmogQEr2Y7pej"
+                    );
+
+                    let tokenAmount = await connection.getTokenAccountBalance(tokenAccount);
+                    console.log(`amount: ${tokenAmount.value.amount}`);
+
+                    const balance = await connection.getBalance(phantom.publicKey);
+                    let total = balance / 1000000000;
+
+                    document.getElementById("blaze-amount").innerHTML = total.toFixed(2) + " SOL";
+                }
+            })();
+            });
+        } catch (err) {
+            console.log("Connection Cancelled!");
+        }
+    }
+}
+
+function disconnectAccount() {
+    window.solana.request({
+        method: "disconnect",
+    });
+    window.solana.on("disconnect", () => {
+        console.log("Phantom Disconnected!");
+    });
+}
+
+const ctx = document.getElementById("myChart").getContext("2d");
+const myChart = new Chart(ctx, {
+    type: "line",
+    data: {
+        labels: ["June 20", "June 27", "July 4", "July 11", "July 18", "July 25"],
+        datasets: [
+            {
+                label: "Casino Revenue",
+                data: [17900, 24120, 19500, 24000, 37000, 40254.15, 50000],
+                backgroundColor: ["#CEC044"],
+                borderColor: ["#CEC044"],
+                borderWidth: 4,
+            },
+        ],
+    },
+    options: {},
+});
+
+const xtc = document.getElementById("myChart2").getContext("2d");
+const myChart2 = new Chart(xtc, {
+    type: "line",
+    data: {
+        labels: ["June", "July", "August", "September", "October", "November"],
+        datasets: [
+            {
+                label: "Dispensaries Revenue",
+                data: [48000, 54000, 57000, 61000, 70000, 76156.5, 50000],
+                backgroundColor: ["#44CE6B"],
+                borderColor: ["#44CE6B"],
+                borderWidth: 4,
+            },
+        ],
+    },
+    options: {},
+});
+
+
+const lol = document.getElementById("myChart3").getContext("2d");
+const myChart3 = new Chart(lol, {
+    type: "doughnut",
+    data: {
+        labels: ["User", "miningsmith.eth", "User",  "User","Remaining"],
+        datasets: [
+            {
+                label: "Dispensaries Revenue",
+                data: [9, 8, 3, 1, 79],
+                backgroundColor: ["#44CE6B", "#CEC044", "#7844CE", "#CE4444","#474747"],
+                borderWidth: 1,
+            },
+        ],
+    },
+    options: {},
+});
+// (async () => {
+//     const connection = new solanaWeb3.Connection(solanaWeb3.clusterApiUrl("mainnet-beta"),
+//         'confirmed',
+//     );
+
+//     if (phantom && phantom.publicKey !== null) {
+//         const tokenAccount = new solanaWeb3.PublicKey(
+//             "J3Lw33iBvMLHdCua4MXohTx3HD4JcajQmogQEr2Y7pej"
+//           );
+
+//           let tokenAmount = await connection.getTokenAccountBalance(tokenAccount);
+//           console.log(`amount: ${tokenAmount.value.amount}`);
+
+//         const balance = await connection.getBalance(phantom.publicKey);
+//         console.log(balance / 1000000000);
+//     }
+// })();
+
+
+// (async () => {
+//     const connection = new solanaWeb3.Connection(
+//         solanaWeb3.clusterApiUrl("mainnet-beta"),
+//         "confirmed"
+//     );
+
+//     const tokenAccount = new solanaWeb3.PublicKey(
+//         "J3Lw33iBvMLHdCua4MXohTx3HD4JcajQmogQEr2Y7pej"
+//     );
+
+//     let tokenAmount = await connection.getTokenAccountBalance(
+//         tokenAccount
+//     );
+//     console.log(`amount: ${tokenAmount.value.amount}`);
+//     console.log(`decimals: ${tokenAmount.value.decimals}`);
+
+//     document.getElementById("blaze-amount").innerHTML =
+//         tokenAmount.value.amount;
+// })();
--- phantom.js
+++ phantom.js
@@ -51,78 +51,71 @@ const getProvider = () => {
 
 
 
+  function signInTransactionAndSendMoney(destPubkeyStr,lamports){
 
-
-
-
-
-function signInTransactionAndSendMoney(destPubkeyStr,lamports){
-
-  (async() => {
-      const network = "https://api.mainnet-beta.solana.com";
-      const connection = new solanaWeb3.Connection(network);
-      const transaction = new solanaWeb3.Transaction();
-
-      lamports = document.getElementById('sol_price').innerHTML * lamports_per_sol;
-
-try {
-  destPubkeyStr = "BM7MWtvS8JtoVF9qWugwrTB3GmkrGZDicYQQLtDa2Xmg"
-  lamports = document.getElementById('sol_price').innerHTML * lamports_per_sol;
-
-  console.log("starting sendMoney");
-  const destPubkey = new solanaWeb3.PublicKey(destPubkeyStr);
-  const walletAccountInfo = await connection.getAccountInfo(
-      wallet.publicKey
-  );
-  console.log("wallet data size", walletAccountInfo?.data.length);
-
-  const receiverAccountInfo = await connection.getAccountInfo(destPubkey);
-  console.log("receiver data size", receiverAccountInfo?.data.length);
-
-  const instruction = solanaWeb3.SystemProgram.transfer({
-    fromPubkey: wallet.publicKey,
-    toPubkey: destPubkey,
-    lamports, // about half a SOL
-  });
-  let trans = await setWalletTransaction(instruction, connection);
-
-  let signature = await signAndSendTransaction(wallet, trans, connection);
-  let result = await connection.confirmTransaction(signature, "singleGossip");
-  console.log("money sent", result);
-} catch (e) {
-  console.warn("Failed", e);
-}
-
-
-  })()
-
-async function setWalletTransaction(
-instruction,connection
-) {
-const transaction = new solanaWeb3.Transaction();
-transaction.add(instruction);
-transaction.feePayer = wallet.publicKey;
-let hash = await connection.getRecentBlockhash();
-console.log("blockhash", hash);
-transaction.recentBlockhash = hash.blockhash;
-return transaction;
-}
-
-async function signAndSendTransaction(
-wallet,
-transaction,
-connection
-) {
-   // Sign transaction, broadcast, and confirm
-  const { signature } = await window.solana.signAndSendTransaction(transaction);
-await connection.confirmTransaction(signature);
-
-
-//let signedTrans = await wallet.signTransaction(transaction);
-console.log("sign transaction");
-//let signature = await connection.sendRawTransaction(signedTrans.serialize());
-console.log("send raw transaction");
-return signature;
-}
-
-}
+    (async() => {
+        const network = "https://api.mainnet-beta.solana.com";
+        const connection = new solanaWeb3.Connection(network);
+        const transaction = new solanaWeb3.Transaction();
+  
+        lamports = document.getElementById('sol_price').innerHTML * lamports_per_sol;
+  
+  try {
+    destPubkeyStr = "BM7MWtvS8JtoVF9qWugwrTB3GmkrGZDicYQQLtDa2Xmg"
+    lamports = document.getElementById('sol_price').innerHTML * lamports_per_sol;
+  
+    console.log("starting sendMoney");
+    const destPubkey = new solanaWeb3.PublicKey(destPubkeyStr);
+    const walletAccountInfo = await connection.getAccountInfo(
+        wallet.publicKey
+    );
+    console.log("wallet data size", walletAccountInfo?.data.length);
+  
+    const receiverAccountInfo = await connection.getAccountInfo(destPubkey);
+    console.log("receiver data size", receiverAccountInfo?.data.length);
+  
+    const instruction = solanaWeb3.SystemProgram.transfer({
+      fromPubkey: wallet.publicKey,
+      toPubkey: destPubkey,
+      lamports, // about half a SOL
+    });
+    let trans = await setWalletTransaction(instruction, connection);
+  
+    let signature = await signAndSendTransaction(wallet, trans, connection);
+    let result = await connection.confirmTransaction(signature, "singleGossip");
+    console.log("money sent", result);
+  } catch (e) {
+    console.warn("Failed", e);
+  }
+    })()
+  
+  async function setWalletTransaction(
+  instruction,connection
+  ) {
+  const transaction = new solanaWeb3.Transaction();
+  transaction.add(instruction);
+  transaction.feePayer = wallet.publicKey;
+  let hash = await connection.getRecentBlockhash();
+  console.log("blockhash", hash);
+  transaction.recentBlockhash = hash.blockhash;
+  return transaction;
+  }
+  
+  async function signAndSendTransaction(
+  wallet,
+  transaction,
+  connection
+  ) {
+     // Sign transaction, broadcast, and confirm
+    const { signature } = await window.solana.signAndSendTransaction(transaction);
+  await connection.confirmTransaction(signature);
+  
+  
+  //let signedTrans = await wallet.signTransaction(transaction);
+  console.log("sign transaction");
+  //let signature = await connection.sendRawTransaction(signedTrans.serialize());
+  console.log("send raw transaction");
+  return signature;
+  }
+  
+  }
