9cf47c29518b50847c65e5e9fed639a2ea82f58a
InvestonerD <solalanddao@gmail.com>
2022-07-30 19:24:36 -0700

    update

--- blaze.js
+++
@@ -1,5 +0,0 @@
-function getValue() {
-    var blaze = document.querySelector('input').value;
-    document.getElementById('convertion_usd').innerHTML = blaze.toString() / 2;
-    console.log(blaze);
-}
--- solanaWeb3js-input.js
+++
@@ -1,1 +0,0 @@
-var solanaWeb3 = require('@solana/web3.js');
